74254e79c95517acc367c410a24ed566b819655f
Vitor Barros <vitornf1221@gmail.com>
2025-06-05 15:38:05 -0300

    Criacao da Classe Game

---
+++ src/main/java/com/dslist/dslist/entities/Game.java
@@ -0,0 +1,95 @@
+package com.dslist.dslist.entities;
+
+import java.util.Objects;
+
+public class Game {
+    //Criar os atributos
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+    public Game(){
+
+    }
+
+
+    public Game(Long id, String title, Integer year, String genre,
+                String platforms, Double score, String imgUrl,
+                String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+
+    // GETS
+
+    public Long getId(){return this.id;}
+
+    public String getTitle() {return title;}
+
+    public Integer getYear() {return year;}
+
+    public String getGenre() {return genre;}
+
+    public String getPlatforms() {return platforms;}
+
+    public Double getScore() {return score;}
+
+    public String getImgUrl() {return imgUrl;}
+
+    public String getShortDescription() {return shortDescription;}
+
+    public String getLongDescription() {return longDescription;}
+
+    //SETS
+
+    public void setId(Long id){this.id = id;}
+
+    public void setTitle(String title) {this.title = title;}
+
+    public void setYear(Integer year) {this.year = year;}
+
+    public void setGenre(String genre) {this.genre = genre;}
+
+    public void setPlatforms(String platforms) {this.platforms = platforms;}
+
+    public void setScore(Double score) {this.score = score;}
+
+    public void setImgUrl(String imgUrl) {this.imgUrl = imgUrl;}
+
+    public void setShortDescription(String shortDescription) {this.shortDescription = shortDescription;}
+
+    public void setLongDescription(String longDescription) {this.longDescription = longDescription;}
+
+
+    // Equals e HashCode
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+
+}
